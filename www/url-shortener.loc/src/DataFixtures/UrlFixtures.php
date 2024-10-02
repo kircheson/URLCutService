cf35d584a2a957e7963b0c2f4acdb64401630dc4
kircheson <kirill.kotlyaren@mail.ru>
2024-10-02 15:42:44 +0500

    Накатил миграцию на основе исходной Entity, добавил Фикстуры

---
+++ www/url-shortener.loc/src/DataFixtures/UrlFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Url;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UrlFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $testUrls = [
+            ['url' => 'https://example.com', 'hash' => 'hash1'],
+            ['url' => 'https://another-example.com', 'hash' => 'hash2'],
+            ['url' => 'https://yetanother-example.com', 'hash' => 'hash3'],
+            ['url' => 'https://test-url.com', 'hash' => 'hash4'],
+            ['url' => 'https://sample-url.com', 'hash' => 'hash5'],
+        ];
+
+        foreach ($testUrls as $data) {
+            $url = new Url();
+            $url->setUrl($data['url']);
+            $url->setHash($data['hash']);
+            $url->setCreatedDate(new \DateTimeImmutable());
+
+            $manager->persist($url);
+        }
+
+        $manager->flush();
+    }
+}
